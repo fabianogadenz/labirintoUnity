e73ed1afea5a729618feb987f140ddbcb91994e5
Fabiano Gadenz <fabianomgadenz@gmail.com>
2019-05-20 12:05:36 -0300

    a

---
+++ Assets/Trap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trap : MonoBehaviour
+{
+
+    public GameObject player;
+    // Start is called before the first frame update
+   
+
+    public void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            //Destroy(player);
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ Assets/spinning.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class spinning : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 1000f;
+
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+
+   public void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            //Destroy(player);
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
